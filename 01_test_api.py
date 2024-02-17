75d5ccfc0f3de9d77c0a14a5e2b6a76c47941b15
fcuamatzi <fjavier.cuamatzi@gmail.com>
2024-02-17 19:02:44 +0000

    se modifica script

--- 01_test_api.py
+++ 01_test_api.py
@@ -1,8 +1,29 @@
+#!/bin/python3
+
+#import requests
+
+#url = 'http://0.0.0.0:3001/convertir'
+#myobj = {"numero": 15,"base_actual": 10,"base_deseada": 8}
+
+#x = requests.post(url, json = myobj)
+
+#print(x.text)
+
+
 import requests
+import json
 
-url = 'http://0.0.0.0:3001/convertir'
-myobj = {"numero": 15,"base_actual": 10,"base_deseada": 8}
 
-x = requests.post(url, json = myobj)
+API_URL = 'http://0.0.0.0:3001/convertir'
 
-print(x.text)
+#Test POST request
+
+response = requests.request("POST", API_URL, json = myobj)
+
+print(response.status_code)
+print(response.text)
+
+if response.status_code != 200 :
+    print('se activo un fail')
+    exit(1)
+
