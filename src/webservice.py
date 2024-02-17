8fb3ba55d3b75047bad1261d13a37f7be797f0d3
fcuamatzi <fjavier.cuamatzi@gmail.com>
2024-02-17 16:30:03 +0000

    regresion y base

--- src/webservice.py
+++ src/webservice.py
@@ -1,6 +1,7 @@
 #!/bin/python
 
 from flask import Flask, request
+import controller
 
 app = Flask(__name__)
 
@@ -18,6 +19,32 @@ def saludoPersona(persona):
 def calculaCuadrado(num):
     resp = num*num
     return 'Respuesta %f desde mi servicio' %resp
+
+
+@app.route('/rlineal', methods=['POST', 'GET'])
+def preRLineal():
+    if request.method == 'POST':
+        data = request.get_json()
+        print(data)
+        vx = data['vx']
+        vy = data['vy']
+        x = data['x']
+        return controller.getRLineal(vx, vy, x)
+
+
+@app.route('/convertir', methods=['POST'])
+def conversion():
+    data = request.json
+    if ('numero' not in data or
+            'base_actual' not in data or
+            'base_deseada' not in data):
+        return json.dumps({"error": "Se requieren los campos 'numero', 'base_actual' y 'base_deseada'."}), 400
+    numero = data['numero']
+    base_actual = data['base_actual']
+    base_deseada = data['base_deseada']
+    return controller.convertir_base(numero,
+            base_actual,
+            base_deseada)
 
 
 @app.route('/test', methods=['POST', 'GET'])
