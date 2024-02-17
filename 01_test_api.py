1581c8171de1095e3c6944e2a217139460bb6650
fcuamatzi <fjavier.cuamatzi@gmail.com>
2024-02-17 19:06:01 +0000

    se modifica script

--- 01_test_api.py
+++ 01_test_api.py
@@ -1,18 +1,10 @@
 #!/bin/python3
 
-#import requests
-
-#url = 'http://0.0.0.0:3001/convertir'
-myobj = {"numero": 15,"base_actual": 10,"base_deseada": 8}
-
-#x = requests.post(url, json = myobj)
-
-#print(x.text)
-
-
 import requests
 import json
 
+
+myobj = {"numero": 15,"base_actual": "10#","base_deseada": 8}
 
 API_URL = 'http://0.0.0.0:3001/convertir'
 
